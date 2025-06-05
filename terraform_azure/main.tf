b101d534d26c76e8d20f56adb8b0da54a85227ae
G-Dsnts <124069692+Gabtorinho25@users.noreply.github.com>
2025-06-05 17:39:46 +0200

    Update main.tf

--- terraform_azure/main.tf
+++ terraform_azure/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
 }
@@ -10,6 +21,9 @@ resource "azurerm_resource_group" "my_rg" {
 resource "azurerm_virtual_network" "my_vnet" {
   name                = "myVnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "East US"
-  resource_group_name = "myResourceGroup"
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
+
+  # 👇 Forcer la dépendance explicite
+  depends_on = [azurerm_resource_group.my_rg]
 }
