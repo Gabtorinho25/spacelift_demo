544c3c53d902430bf4778ecf943af1beda31de3f
G-Dsnts <124069692+Gabtorinho25@users.noreply.github.com>
2025-06-05 17:00:05 +0200

    Update main.tf

--- terraform_azure/main.tf
+++ terraform_azure/main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "34980b86-905c-48be-8e76-fd0debe129ab"
-  client_id       = "6403fc16-323b-4817-8583-2acf2c0876bd"
-  client_secret   = "pnP8Q~mXGjusA-RKBGOH6d2DHYRvhLSY3LWs7bvD"
-  tenant_id       = "108bc864-cdf5-4ec3-8b7c-4eb06be1b41d"
+  subscription_id = "1c4e8749-9651-495c-a455-309e1201bfb1"
+  client_id       = "d631accc-7fbd-4e4d-94e6-aea947e2cb7b"
+  client_secret   = "gWy8Q~ILoahGHWq5TwRhDM2GtaIV1~ZX8iirOaon"
+  tenant_id       = "fee747b5-9cea-4ce8-b5e7-7b34cb1be686"
 }
 
 resource "azurerm_virtual_network" "my_vnet" {
