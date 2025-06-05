541b4bb32a1a68eb4316982f1cdd39970b41c211
G-Dsnts <124069692+Gabtorinho25@users.noreply.github.com>
2025-06-05 16:56:36 +0200

    Update main.tf

--- terraform_azure/main.tf
+++ terraform_azure/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 
   subscription_id = "34980b86-905c-48be-8e76-fd0debe129ab"
   client_id       = "6403fc16-323b-4817-8583-2acf2c0876bd"
-  client_secret   = "1425f552-984a-4c4f-b029-eef8e64e7f67"
+  client_secret   = "pnP8Q~mXGjusA-RKBGOH6d2DHYRvhLSY3LWs7bvD"
   tenant_id       = "108bc864-cdf5-4ec3-8b7c-4eb06be1b41d"
 }
 
