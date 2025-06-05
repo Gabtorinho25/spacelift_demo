50c04154a9ae18da49bd2458138d171e2dfc0e49
G-Dsnts <124069692+Gabtorinho25@users.noreply.github.com>
2025-06-05 17:37:03 +0200

    Update main.tf

--- terraform_azure/main.tf
+++ terraform_azure/main.tf
@@ -2,6 +2,11 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "my_rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
 resource "azurerm_virtual_network" "my_vnet" {
   name                = "myVnet"
   address_space       = ["10.0.0.0/16"]
