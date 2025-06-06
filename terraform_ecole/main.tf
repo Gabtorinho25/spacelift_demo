d40de0c005d526cae1758c9b164732174da3310b
G-Dsnts <124069692+Gabtorinho25@users.noreply.github.com>
2025-06-06 10:03:49 +0200

    Update main.tf_tp_final

--- terraform_ecole/main.tf
+++ terraform_ecole/main.tf
@@ -17,38 +17,92 @@ provider "azurerm" {
   tenant_id       = "108bc864-cdf5-4ec3-8b7c-4eb06be1b41d"
 }
 
-variable "resource_group_name" {
-  type        = string
-  description = "Nom du groupe de ressources Azure"
-  default     = "RG-azure-td"
-}
-
-variable "location" {
-  type        = string
-  description = "Région Azure où les ressources seront déployées"
-  default     = "eastus"
-}
-
-variable "vnet_name" {
-  type        = string
-  description = "Nom du virtual network (VNet)"
-  default     = "vnet-td"
-}
-
-variable "address_space" {
-  type        = list(string)
-  description = "Plage d'adresses pour le VNet"
-  default     = ["10.0.0.0/16"]
-}
-
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "bastion-rg"
   location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  address_space       = var.address_space
-  location            = azurerm_resource_group.rg.location
+  name                = "bastion-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "bastion-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "bastion_ip" {
+  name                = "bastion-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_security_group" "bastion_nsg" {
+  name                = "bastion-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH-From-My-IP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.my_public_ip
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "bastion_nic" {
+  name                = "bastion-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.bastion_ip.id
+  }
+
+  network_security_group_id = azurerm_network_security_group.bastion_nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "bastion_vm" {
+  name                = "bastion-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.bastion_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.bastion_ssh_pubkey
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
 }
