9e6446aa69f38e6390519882b93aa3c03c5cf2b4
G-Dsnts <124069692+Gabtorinho25@users.noreply.github.com>
2025-06-06 13:06:15 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,49 @@
 variable "aws_region" {
-  description = "AWS region to deploy into"
+  description = "AWS region"
   type        = string
-  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = "Name for the SSH key pair"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key content"
+  type        = string
+}
+
+variable "my_public_ip" {
+  description = "Your IP address (x.x.x.x/32) to allow SSH access"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "app_subnet_cidr" {
+  description = "CIDR block for the app subnet"
+  type        = string
+}
+
+variable "db_subnet_cidr" {
+  description = "CIDR block for the DB subnet"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
 }
